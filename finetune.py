aca815fe66347884ac23eb2afa6d22544aff4f7b
ochwo laban <labanochwo@gmail.com>
2026-07-13 22:38:07 +0300

    Made changes

--- finetune.py
+++ finetune.py
@@ -18,6 +18,11 @@ from utils.utils import print_arguments, make_inputs_require_grad, add_arguments
 from evaluate import load
 wer_metric = load("wer")
 
+import transformers
+transformers.logging.set_verbosity_error()
+import logging
+logging.getLogger("transformers").setLevel(logging.ERROR)
+
 parser = argparse.ArgumentParser(description=__doc__)
 add_arg = functools.partial(add_arguments, argparser=parser)
 add_arg("train_data",    type=str, default="dataset/train.json",       help="训练数据集的路径")
